cc0763edae9f99e29bc7d0500286bc9eef587787
Olga Sotiriadi <67987697+olgasotiriadi@users.noreply.github.com>
2020-08-11 20:35:37 -0700

    Updates

---
+++ precache-manifest.ca7b7f3ac2e192ba3c1d8e18ef5fdeed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05351ae480b4d984a14bf79daa82f03e",
+    "url": "/kanaban_august2020/index.html"
+  },
+  {
+    "revision": "db23e5342ff828b9e88b",
+    "url": "/kanaban_august2020/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc4a7c71b41a7376fc3e",
+    "url": "/kanaban_august2020/static/js/2.37f42d80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanaban_august2020/static/js/2.37f42d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db23e5342ff828b9e88b",
+    "url": "/kanaban_august2020/static/js/main.5711c4e0.chunk.js"
+  },
+  {
+    "revision": "9e83ffc20fc40847c2fd",
+    "url": "/kanaban_august2020/static/js/runtime-main.fea0c2ff.js"
+  }
+]);
